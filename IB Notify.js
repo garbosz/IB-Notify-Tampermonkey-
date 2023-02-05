26d819fb67ab78de18683028cfcb2cfbd69ae4fb
Garbosz <zacgarbos@gmail.com>
2023-02-04 21:18:42 -0500

    first fully functional version

--- IB Notify.js
+++ IB Notify.js
@@ -10,12 +10,29 @@
 // @grant        GM_notification
 // @run-at document-end
 // ==/UserScript==
+let processed=[];
+  var button = document.createElement("button");
+  button.innerHTML = "Reset";
+  button.style.padding = "10px 20px";
+  button.style.backgroundColor = "#4CAF50";
+  button.style.color = "white";
+  button.style.border = "none";
+  button.style.borderRadius = "4px";
+  button.style.cursor = "pointer";
+  document.body.appendChild(button);
 
+  button.addEventListener ("click", function() {
+    processed = [];
+    console.log("Processed array reset to empty:", processed);
+  });
 // Function to display the pop-up dialog box
 setTimeout(5000);
-function displayPopup(data) {
+
+function displayPopup(vol) {
+    if(vol.textContent > 0){
     var current = new Date()
-  alert("New Manifest! @"+current.toLocaleTimeString()+"\nVRID: "+data[0]+"\nVolume on board: "+data[1]);
+  alert("New Manifest! @"+current.toLocaleTimeString()+"\nVRID: "+vol.firstChild.dataset.vrid+"\nVolume on board: "+vol.textContent);
+    }
 }
 
 function fetchData() {
@@ -23,20 +40,33 @@ function fetchData() {
     console.log(vol)
     console.log(vol[0].textContent)//this is the volume
     console.log(vol[0].firstChild.dataset.vrid)//this is the VRID
- var dashboardElement=vol[0].innerHTML
+ var dashboardElement=vol
 
   return dashboardElement;
 }
 
-function findNew(data){
-   var message=data
-    return message
+function getVolumeData(vol) {
+  let output = [];
+  const maxLength = 30;
+  for (let i = 0; i < vol.length; i++) {
+    if (vol[i].textContent.trim().length > 0 && !processed.includes(vol[i])) {
+      output.push([vol[i].firstChild.dataset.vrid, vol[i].textContent.trim()]);
+      processed.push(vol[i]);
+        console.log(vol[i]);
+      if (processed.length > maxLength) {
+        processed.shift();
+      }
+      displayPopup(vol[i]);
+    }
+  }
+  return output;
 }
+
 
 // Fetch the data every 5 minutes
 setInterval(function() {
   var dataTable = fetchData();//get data build table
-  findNew(dataTable)//chug through data if new manifest found, add to blacklist and call popup
+  getVolumeData(dataTable)//chug through data if new manifest found, add to blacklist and call popup
 }, 300000); // 5 minutes in milliseconds
 //}, 3000); // 3 seconds in milliseconds for testing
 
